5e1d69ece1c3518c279523d3c1ae56b01024cfde
Andrei Brancalião <108850397+Andrei-Brancaliao@users.noreply.github.com>
2022-08-26 19:12:18 -0300

    Task/01 (#1)
    * Automacao web
    
    * Finalizacao da verificacao da lista

--- web.final.test/src/test/java/utils/Utils.java
+++ web.final.test/src/test/java/utils/Utils.java
@@ -1,5 +1,47 @@
 package utils;
 
+import java.util.List;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import stepDefinitions.Hooks;
+
 public class Utils {
 
+	protected static WebDriver driver;
+	
+	public void esperarElemento(WebElement elemento) {
+		driver = Hooks.getDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOf(elemento));
+	}
+	
+	public void esperarListaElementos(List<WebElement> listaWebElement) {
+		driver = Hooks.getDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfAllElements(listaWebElement));
+	}
+	
+	public void esperarMultiplosElementos(WebElement ... elemento) {
+		driver = Hooks.getDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfAllElements(elemento));
+	}
+	
+	public void selecionarNaLista(WebElement elemento, String texto) {
+		driver = Hooks.getDriver();
+		Select dropDown = new Select(elemento);
+		dropDown.selectByVisibleText(texto);
+	}
+	
+	public void scrollDown() {
+		new Actions(driver)
+        .sendKeys(Keys.PAGE_DOWN)
+        .perform();
+	}
 }
